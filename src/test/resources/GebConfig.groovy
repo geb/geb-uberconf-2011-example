ba38963567e0a972cf1f08c81b98caa7f3bba97b
Peter Niederwieser <pniederw@gmail.com>
2011-07-15 14:30:34 -0600

    improved IDEA project generation; simplified Geb configuration; removed support for running tests with Chrome

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -5,28 +5,11 @@
 */
 
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.chrome.ChromeDriver
 
 driver = { new FirefoxDriver() } // default
+reportsDir = "build/reports/geb"
+baseUrl = "http://uberconf.com/conference/denver/2011/07/"
 
 waiting {
 	timeout = 2
 }
-
-environments {
-	
-	// run via “./gradlew chromeTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
-	
-	// run via “./gradlew firefoxTest”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
-		
-	}
-
-}
-
-// To run the tests with all browsers just run “./gradlew test”
